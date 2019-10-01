337782de398d4cb0702affb2807c6451d5c5ebae
kojiro.ueda <mxcpy608@hotmail.com>
2019-10-01 18:17:18 +0900

    Improve test code file dir.

--- README.spec.js
+++ README.spec.js
@@ -3,7 +3,7 @@ const { parse } = require("@power-doctest/markdown");
 const { readFileSync } = require("fs");
 const { join } = require("path");
 
-const contentPath = join(__dirname, "../README.md");
+const contentPath = join(__dirname, "./README.md");
 const content = readFileSync(contentPath, "utf-8");
 const parsedResults = parse({ content, filePath: contentPath });
 describe("doctest:markdown", () => {
@@ -11,7 +11,7 @@ describe("doctest:markdown", () => {
     const { location } = parsedCode;
     it(`README.md:${location.start.line}-${location.end.line}`, async () => {
       try {
-        await test({ ...parsedCode, code: `const { select, createTable, insertInto } = require("${join(__dirname, "../index")}"); ${parsedCode.code}` });
+        await test({ ...parsedCode, code: `const { select, createTable, insertInto } = require("${join(__dirname, "./index")}"); ${parsedCode.code}` });
       } catch(e) {
         console.error(
           `StrictEvalError: strict eval is failed at strictEval (README.md:${location.start.line}:${location.start.column})`
