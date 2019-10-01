69369167407c3fcf5274151a912e7f0a485dbf87
kojiro.ueda <mxcpy608@hotmail.com>
2019-10-01 15:36:29 +0900

    Enable to build when pretest, prepublish.

--- test/README.spec.js
+++ test/README.spec.js
@@ -11,7 +11,7 @@ describe("doctest:markdown", () => {
     const { location } = parsedCode;
     it(`README.md:${location.start.line}-${location.end.line}`, async () => {
       try {
-        await test({ ...parsedCode, code: `const { select, createTable, insertInto } = require("./index"); ${parsedCode.code}` });
+        await test({ ...parsedCode, code: `const { select, createTable, insertInto } = require("${join(__dirname, "../index")}"); ${parsedCode.code}` });
       } catch(e) {
         console.error(
           `StrictEvalError: strict eval is failed at strictEval (README.md:${location.start.line}:${location.start.column})`
