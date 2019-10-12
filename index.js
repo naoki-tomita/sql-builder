bb6ced78e5dfabddacd5efa7a3d78c0087dcbac9
kojiro.ueda <mxcpy608@hotmail.com>
2019-10-12 23:08:14 +0900

    Bugfix auto increment syntax.

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ function uniqueFactory(prefix) {
 }
 function autoIncrementFactory(prefix) {
     return function () {
-        var sql = prefix + " AUTOINCREMENT";
+        var sql = prefix + " AUTO INCREMENT";
         return nextable(sql);
     };
 }
